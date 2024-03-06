03033d325fc0755ec56f41d51a37f88200e5fd4f
mwahkeymer <audreymwakima@gmail.com>
2024-03-06 12:59:09 +0000

    correct no 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,15 +1,6 @@
 export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
-    // Simulate a condition where the file cannot be processed
-    if (fileName.toLowerCase().includes('error')) {
-      const errorMessage = `${fileName} cannot be processed`;
+	  const errorMessage = `${fileName} cannot be processed`;
       reject(new Error(errorMessage));
-    } else {
-      // Simulate a successful upload
-      resolve({
-        status: 200,
-        body: `Photo ${fileName} uploaded successfully`,
-      });
-    }
   });
 }
