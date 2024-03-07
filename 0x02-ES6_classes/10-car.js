c07d170b5dd32c8a3666e3ad7f43b0fff24fd725
mwahkeymer <audreymwakima@gmail.com>
2024-03-07 09:59:33 +0000

    querry 10cort

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,59 +1,16 @@
-// Car.js
-const cloneCarSymbol = Symbol('cloneCar');
-
-/**
- * Class representing a Car.
- */
 export default class Car {
-  /**
-   * Create a Car.
-   * @param {string} [brand] - The brand of the car.
-   * @param {string} [motor] - The motor type of the car.
-   * @param {string} [color] - The color of the car.
-   */
   constructor(brand, motor, color) {
-    // Store attributes in underscore attribute version
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-
-    // Create a unique symbol for the cloneCar method
-    this[cloneCarSymbol] = function () {
-      // Return a new object of the class with the same attributes
-      return new Car(this._brand, this._motor, this._color);
-    };
   }
 
-  /**
-   * Get the brand of the car.
-   * @returns {string} - The brand of the car.
-   */
-  get brand() {
-    return this._brand;
+  static get [Symbol.species]() {
+    return this;
   }
 
-  /**
-   * Get the motor type of the car.
-   * @returns {string} - The motor type of the car.
-   */
-  get motor() {
-    return this._motor;
-  }
-
-  /**
-   * Get the color of the car.
-   * @returns {string} - The color of the car.
-   */
-  get color() {
-    return this._color;
-  }
-
-  /**
-   * Clone the car and return a new object.
-   * @returns {Car} - A new object of the Car class.
-   */
   cloneCar() {
-    // Return a new object with the same attributes or default to undefined if not provided
-    return new Car(this._brand, this._motor, this._color);
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
