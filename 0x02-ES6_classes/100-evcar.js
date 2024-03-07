a74e697b10694dca09480ca85350a7949753dd54
mwahkeymer <audreymwakima@gmail.com>
2024-03-07 10:12:24 +0000

    last last querry

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,7 +1,33 @@
-import EVCar from './100-evcar';
+// EVCar.js
+import Car from "./10-car";
 
-const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
-console.log(ec1);
+export default class EVCar extends Car {
+  /**
+   * Create an Electric Vehicle (EV) Car.
+   * @param {string} brand - The brand of the car.
+   * @param {string} motor - The motor type of the car.
+   * @param {string} color - The color of the car.
+   * @param {string} range - The range of the electric car.
+   */
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-const ec2 = ec1.cloneCar();
-console.log(ec2);
+  /**
+   * Get the range of the electric car.
+   * @returns {string} - The range of the electric car.
+   */
+  get range() {
+    return this._range;
+  }
+
+  /**
+   * Clone the electric car and return a new instance of Car.
+   * @returns {Car} - A new instance of Car.
+   */
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
+  }
+}
