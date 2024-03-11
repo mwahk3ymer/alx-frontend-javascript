3c38b0d09cddcf82bb675d6931a21bc59289bf37
mwahkeymer <audreymwakima@gmail.com>
2024-03-11 06:52:43 +0000

    function named getListStudents that returns an array of objects.

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+}
+
+// Export the function
+export default getListStudents;
